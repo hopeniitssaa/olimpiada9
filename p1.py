3d80d425a594167eaa2fdd3ab9609e6858a28d93
Băbuță Luminița <70962140+hopeniitssaa@users.noreply.github.com>
2020-12-07 12:42:09 +0300

    Add files via upload

---
+++ p1.py
@@ -0,0 +1,7 @@
+x = int(input("dati numarul de pasari: "))
+gaini = x // 2
+rate = gaini // 4
+gaste = x - gaini - rate
+print(f'Numarul de gaini este {gaini}')
+print(f'Numarul de rate este {rate}')
+print(f'Numarul de gaste este {gaste}')
