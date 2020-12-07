949845063ba2b42a10eb601da6055477b9fb16f9
Băbuță Luminița <70962140+hopeniitssaa@users.noreply.github.com>
2020-12-07 22:57:20 +0300

    Add files via upload

---
+++ olimp4.py
@@ -0,0 +1,8 @@
+print("Lungimea gardului lotului de pamant patrat este de 48 metri")
+perim_patr = 48
+lat_patr = perim_patr // 4
+lat_drep = 9
+aria_patr = lat_patr ** 2
+lun_drep = aria_pat // lat_drep
+perim_drep = (2 * lun_drep) + (2 * lat_drep)
+print("Lungimea gardului lotului de pamaant dreptunghic este de",perim_drep,"metri")
